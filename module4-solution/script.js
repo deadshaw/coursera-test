ae97ca7fedde1b28460ad8ed3cc664dbc9a4876a
deadshaw <120623044+deadshaw@users.noreply.github.com>
2023-01-09 13:11:55 +0530

    Add files via upload

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function(){
-    var name = ["Popo", "Momo", "Jojo", "Gogo", "Jozo", "Bozo"];
+    var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for(var i = 0; i<name.length; i++){
         var firstLetter = name[i].charAt(0).toLowerCase();
         if (firstLetter === 'j'){
