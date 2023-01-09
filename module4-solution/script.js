f59e6009de715935ad272e43b917d383eff3b330
deadshaw <120623044+deadshaw@users.noreply.github.com>
2023-01-09 08:19:26 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,14 @@
+(function(){
+    var name = ["Popo", "Momo", "Jojo", "Gogo", "Jozo", "Bozo"];
+    for(var i = 0; i<name.length; i++){
+        var firstLetter = name[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j'){
+            byeSpeaker.speak(name[i]);
+        } 
+        else{
+            helloSpeaker.speak(name[i]);
+        }
+    }
+}
+)();
 
